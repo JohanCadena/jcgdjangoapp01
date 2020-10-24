d2bacbf048343d24755951e9be97c626140e0a10
Johan Cadena García <johan7cg@gmail.com>
2020-10-23 19:57:47 -0500

    2do avance de proyecto 22-octubre-2020

---
+++ trabajogrupal/article/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Categoria')
+    description = models.CharField(max_length=300, verbose_name='Descripcion')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name= 'Categoria'
+        verbose_name_plural= 'Categorias'
+
+class Articulo(models.Model):
+    categoria_id = models.ForeignKey(Categoria, on_delete=models.CASCADE, verbose_name='Categoria') 
+    name = models.CharField(max_length=255, verbose_name='Nombre')
+    stock = models.CharField(max_length=300, verbose_name='Piezas')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Precio')
+
+    def __str__(self):
+        return "{0}".format(self.name)
+
+    class Meta:
+        verbose_name= 'Articulo'
+        verbose_name_plural= 'Articulos'
