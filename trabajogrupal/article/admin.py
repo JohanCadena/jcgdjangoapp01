6d38513995f48dc35a61756f5f37aa3c6785335a
Johan Cadena García <johan7cg@gmail.com>
2020-10-29 03:52:03 -0600

    Tercer avance de proyecto 29-10-2020

--- trabajogrupal/article/admin.py
+++ trabajogrupal/article/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Categoria, Articulo
-from .models import Articulo
+from .models import Categoria, Articulo, Venta
 
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Articulo)
+admin.site.register(Venta)
--- trabajogrupal/article/models.py
+++ trabajogrupal/article/models.py
@@ -24,3 +24,17 @@ class Articulo(models.Model):
     class Meta:
         verbose_name= 'Articulo'
         verbose_name_plural= 'Articulos'
+
+class Venta(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nombre Cliente')
+    articulo_id = models.ForeignKey(Articulo, on_delete=models.CASCADE, verbose_name='Articulo') 
+    stock = models.CharField(max_length=300, verbose_name='Piezas')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Precio')
+    total = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Total')
+
+    def __str__(self):
+        return "{0}".format(self.name)
+
+    class Meta:
+        verbose_name= 'Venta'
+        verbose_name_plural= 'Nueva Venta'
